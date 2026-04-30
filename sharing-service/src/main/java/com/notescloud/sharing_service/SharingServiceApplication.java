2253229d13dea9077ed982179600739f191562dd
Mihael Mihaylov <mishoni04@abv.bg>
2026-04-30 22:40:14 +0300

    Hello world endpoint

--- sharing-service/src/main/java/com/notescloud/sharing_service/SharingServiceApplication.java
+++ sharing-service/src/main/java/com/notescloud/sharing_service/SharingServiceApplication.java
@@ -2,12 +2,18 @@ package com.notescloud.sharing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class,
+	HibernateJpaAutoConfiguration.class,
+	FlywayAutoConfiguration.class
+})
 public class SharingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SharingServiceApplication.class, args);
 	}
-
 }
