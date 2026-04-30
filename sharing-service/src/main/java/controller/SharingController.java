ee27d3094a373edfd57e5051882649cd70395706
Mihael Mihaylov <mishoni04@abv.bg>
2026-04-30 23:02:12 +0300

    last

--- sharing-service/src/main/java/controller/SharingController.java
+++ sharing-service/src/main/java/controller/SharingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TodoController {
+public class SharingController {
 
     @GetMapping("/Hello World")
     public String helloWorld() {
