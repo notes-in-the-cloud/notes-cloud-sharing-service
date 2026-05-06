e6da0b7205bdc891a34268f423419b7a9353b909
Mihael Mihaylov <mishoni04@abv.bg>
2026-05-06 21:17:34 +0300

    added properties

--- sharing-service/src/main/java/com/notescloud/sharing_service/SharingServiceApplication.java
+++ sharing-service/src/main/java/com/notescloud/sharing_service/SharingServiceApplication.java
@@ -1,9 +1,10 @@
 package com.notescloud.sharing_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SharingServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SharingServiceApplication.class, args);
 	}
